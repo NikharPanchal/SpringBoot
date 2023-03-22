07da5910c0581e4cab16e7ea12a201a2e6dc5343
NikharPanchal <Panchalnikhar4920@gmail.com>
2023-03-22 23:41:10 +0530

    initial commit

---
+++ SpringUserRestAPI/src/main/java/com/aspire/user/SpringUserRestApiApplication.java
@@ -0,0 +1,14 @@
+package com.aspire.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserRestApiApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUserRestApiApplication.class, args);
+	}
+
+}
