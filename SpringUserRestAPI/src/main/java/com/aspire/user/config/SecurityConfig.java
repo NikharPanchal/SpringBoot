ee296f226c9c808fd2b37349ebc60fedd76ae9f2
NikharPanchal <panchalnikhar4920@gmail.com>
2023-03-29 18:45:31 +0530

    changes in configuration

--- SpringUserRestAPI/src/main/java/com/aspire/user/config/SecurityConfig.java
+++ SpringUserRestAPI/src/main/java/com/aspire/user/config/SecurityConfig.java
@@ -5,18 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.aspire.user.service.UserService;
-import com.aspire.user.utils.Users;
 
 import lombok.AllArgsConstructor;
 
@@ -43,15 +38,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 //		http.authorizeHttpRequests().anyRequest().authenticated().and().httpBasic();
-		http.authorizeHttpRequests().antMatchers("/users","/token","/save","/h2-console/**","/user/*").permitAll().anyRequest().authenticated().and().httpBasic();
-//		http.csrf().disable().cors().disable().authorizeRequests().antMatchers("/token").permitAll().anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//		http.csrf().disable();
+//		http.headers().frameOptions().disable(); 
+
+		
+		http.authorizeHttpRequests().antMatchers("/users","/token","/save","/user/*").permitAll().anyRequest().authenticated().and().httpBasic();
 		http.csrf().disable();
-		http.headers().frameOptions().disable();
+
+		
+//		http.csrf().disable().cors().disable().authorizeRequests().antMatchers("/token").permitAll().anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+//		http
+//			.csrf()
+//			.disable()
+//			.authorizeRequests()
+//			.anyRequest()
+//			.authenticated()
+//			.and()
+//			.httpBasic();
+		
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password(this.passwordEncoder().encode("admin")).roles("USER");
+		auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("admin")).roles("USER");
+//		.and().withUser("user").password("admin").roles("USER","");
 		auth.userDetailsService(userService)
 		.passwordEncoder(passwordEncoder());
 	}
